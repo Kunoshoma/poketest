be0951a2a2a8b5b07258bfadd67cd0832b931751
RolfeePCFX <kuno.shoma@gmail.com>
2022-07-26 18:49:27 +0900

    Add VMark

--- src/Player.jsx
+++ src/Player.jsx
@@ -1,14 +1,16 @@
 import React from "react";
+import vstar from "./VASTAR_marker.png";
 
 class Player extends React.Component {
   constructor(props) {
     super(props);
-    this.state = { value: "", ene:"" ,hp:""};
+    this.state = { value: "", ene:"" ,hp:"",clicked:false};
 
     this.handleChange = this.handleChange.bind(this);
     this.handleSubmit = this.handleSubmit.bind(this);
     this.handleEne = this.handleEne.bind(this);
     this.handleHp = this.handleHp.bind(this);
+    this.imageClick = this.imageClick.bind(this);
   }
 
   handleChange(event) {
@@ -24,6 +26,12 @@ class Player extends React.Component {
   handleSubmit(event) {
     event.preventDefault();
   }
+  imageClick(event){
+    this.setState({
+      clicked: (event.target.value = !event.target.value),
+    });
+  }
+  line1 = <div className="gradline"></div>
 
   render() {
     return (
@@ -39,11 +47,22 @@ class Player extends React.Component {
         <h1 className="maintext">{this.state.hp}</h1>
         <form onSubmit={this.handleSubmit}>
           <label className="param">
+            <p className="discription">VSTAR</p>
+            <div className="gradline"></div>
+            <img
+              className={
+                this.state.clicked ? "offvstarMarker" : "onvstarMarker"
+              }
+              src={vstar}
+              alt=""
+              onClick={this.imageClick}
+            />
             <input
               type="text"
               value={this.state.value}
               onChange={this.handleChange}
               placeholder="プレイヤー名"
+              className="inputconfig"
             />
             <form onSubmit={this.handleSubmit}>
               <input
@@ -54,6 +73,7 @@ class Player extends React.Component {
                 onChange={this.handleEne}
                 placeholder="サイド枚数"
                 name="side"
+                className="inputconfig"
               />
             </form>
             <form onSubmit={this.handleSubmit}>
@@ -66,6 +86,7 @@ class Player extends React.Component {
                 onChange={this.handleHp}
                 placeholder="残りHP"
                 name="hpleft"
+                className="inputconfig"
               />
             </form>
           </label>
