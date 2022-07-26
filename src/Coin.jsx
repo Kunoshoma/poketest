ce769f7204412fe2786c9385fba9f0f998ee9539
RolfeePCFX <kuno.shoma@gmail.com>
2022-07-26 17:07:36 +0900

    Fix coin

--- src/Coin.jsx
+++ src/Coin.jsx
@@ -2,20 +2,17 @@ import React, { useState } from "react" ;
 import omote from "./omote.png"
 import ura from "./ura.png";
 
-function Coin(){
-  let [num,setNum] = useState(0);
-  if(num < 0.5) {
+
+
+function Coin() {
+  let [num, setNum] = useState(0);
     return (
       <div>
-        <img src={omote} alt="" className="coin" />
-        <button className="bn29" onMouseDown={() => setNum((num = Math.random))}>コイン</button>
-        <p>{num}</p>
-      </div>
-    );
-  }else{
-    return (
-      <div>
-        <img src={ura} alt="" className="coin" />
+        {num < 0.5 ? (
+          <img src={omote} alt="" className="coin" />
+        ) : (
+          <img src={ura} alt="" className="coin" />
+        )}
         <button
           className="bn29"
           onMouseDown={() => setNum((num = Math.random))}
@@ -25,9 +22,6 @@ function Coin(){
         <p>{num}</p>
       </div>
     );
-  }
-  
-  
 }
 
 
